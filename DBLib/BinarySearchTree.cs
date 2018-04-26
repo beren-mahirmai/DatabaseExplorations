aa6a9e4f8305a426269338b065ed162a1e9d8198
beren-mahirmai <beren.mahirmai@gmail.com>
2018-04-26 07:59:28 -0700

    Exploring the best way to handle BST adds

--- DBLib/BinarySearchTree.cs
+++ DBLib/BinarySearchTree.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 namespace DBLib
 {
+    // This is a recursive algorithm, but recursion is bad in languages without tail call optimization.
+    // We're using an iterative approach to avoid OutOfMemoryException in very large sets.
     public class BinarySearchTree<T>
     {
         private readonly IComparer<T> Comparer;
@@ -14,12 +16,56 @@ namespace DBLib
             Comparer = comparer;
         }
 
-        public void Add(T value) {
+        public void Add(T newValue) {
             if(Root == null) {
-                Root = new Node(value, Comparer);
+                Root = new Node(newValue);
                 return;
             } 
-            Root.Add(value);
+            Node current = Root;
+            while(true) {
+                if(Comparer.Compare(current.Value, newValue) > 0) {
+                    // current.Value > newValue
+                    if(current.LowerValues == null) {
+                        current.LowerValues = new Node(newValue);
+                        break;
+                    } else {
+                        current = current.LowerValues;
+                        continue;
+                    }
+                } else {
+                    // current.Value <= newValue
+                    if(current.HigherValues == null) {
+                        current.HigherValues = new Node(newValue);
+                        break;
+                    } else {
+                        current = current.HigherValues;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private static Node FindClosestLeaf(Node root, T value, IComparer<T> comparer) {
+            Node current = root;
+            while(true) {
+                if(comparer.Compare(current.Value, value) > 0) {
+                    // current.Value > value
+                    if(current.LowerValues == null) {
+                        return current;
+                    } else {
+                        current = current.LowerValues;
+                        continue;
+                    }
+                } else {
+                    // current.Value <= value
+                    if(current.HigherValues == null) {
+                        return current;
+                    } else {
+                        current = current.HigherValues;
+                        continue;
+                    }
+                }
+            }        
         }
 
         public Boolean Contains(T obj) {
@@ -35,29 +81,8 @@ namespace DBLib
             public Node LowerValues;
             public Node HigherValues;
 
-            private readonly IComparer<T> Comparer;
-
-            public Node(T value, IComparer<T> comparer) {
+            public Node(T value) {
                 Value = value;
-                Comparer = comparer;
-            }
-
-            public void Add(T newValue) {
-                if(Comparer.Compare(Value, newValue) > 0) {
-                    // Value > newValue
-                    if(LowerValues == null) {
-                        LowerValues = new Node(newValue, Comparer);
-                    } else {
-                        LowerValues.Add(newValue);
-                    }
-                } else {
-                    // Value <= newValue
-                    if(HigherValues == null) {
-                        HigherValues = new Node(newValue, Comparer);
-                    } else {
-                        HigherValues.Add(newValue);
-                    }
-                }
             }
         }
     }
