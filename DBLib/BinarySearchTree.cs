6d2239b9638805850a9fedc53c332fb307c94964
Beau Kennedy <bmkennedy@huronconsultinggroup.com>
2018-04-27 17:23:41 -0700

    Built out some logic for walking the tree

--- DBLib/BinarySearchTree.cs
+++ DBLib/BinarySearchTree.cs
@@ -19,16 +19,20 @@ namespace DBLib
         }
 
         public void Add(T newValue) {
-            Count++;
             if(Root == null) {
+                Count = 1;
                 Root = new Node(newValue);
                 return;
             } 
             Node current = Root;
             while(true) {
-                if(Comparer.Compare(current.Value, newValue) > 0) {
+                Int32 compareResult = Comparer.Compare(current.Value, newValue);
+                if(compareResult == 0) {
+                    break; // already in the tree, so no op
+                } else if(compareResult > 0) {
                     // current.Value > value
                     if(current.LowerValues == null) {
+                        Count++;
                         current.LowerValues = new Node(newValue);
                         break;
                     } else {
@@ -36,8 +40,9 @@ namespace DBLib
                         continue;
                     }
                 } else {
-                    // current.Value <= value
+                    // current.Value < value
                     if(current.HigherValues == null) {
+                        Count++;
                         current.HigherValues = new Node(newValue);
                         break;
                     } else {
@@ -63,7 +68,7 @@ namespace DBLib
                         continue;
                     }
                 } else {
-                    // current.Value <= value
+                    // current.Value < value
                     if(current.HigherValues == null) {
                         return false;
                     } else {
@@ -76,18 +81,59 @@ namespace DBLib
 
         public T[] ToSortedArray() {
             return new T[0];
-            // T[] result = new T[Count];
-            // if(Root == null) {
-            //     return result;
-            // }
-            // Int32 index = 0;
-            // Node current = Root;
-            // while(true) {
-                
-            // }
+            T[] result = new T[Count];
+            if(Root == null) {
+                return result;
+            }
+            Int32 index = 0;
+            Node current = Root;
+            while(true) {
+                // if index == Count
+                break;
+                // if LowerValues.value is lowest, go down that path
+                // if current.Value is lowest, take this value
+                // if HigherValues.value is lowest, go down that path 
+            }
         }
 
-        private class Node {
+        public Path DeterminePath(Node node, T testValue) {
+            if(node.LowerValues != null && testValue.Equals(default(T))) {
+                // LowerValues path exists and we haven't settled on a first value yet
+                return Path.Lower;
+            }
+            if(node.LowerValues != null && Comparer.Compare(node.LowerValues.Value, testValue) > 0) {
+                // LowerValues path exists and it hasn't been traversed yet (higher than test value)
+                return Path.Lower;
+            }
+            if(node.LowerValues == null && testValue.Equals(default(T))) {
+                // No LowerValues and we haven't picked the first item, so this is that item
+                return Path.Current;
+            }
+            if(Comparer.Compare(node.Value, testValue) >= 0) {
+                // No LowerValues or LowerValues are already traversed and we haven't added the current item yet
+                return Path.Current;
+            }
+            if(node.HigherValues == null) {
+                // We have already handled the LowerValues path and current value, and there is no higher path
+                return Path.Back;
+            }
+            if(node.HigherValues != null && Comparer.Compare(node.HigherValues.Value, testValue) > 0) {
+                // There is a higher path and we haven't traversed it yet
+                return Path.Higher;
+            }
+
+            // We have already traversed the low, current, and high paths
+            return Path.Back;
+        }
+
+        public enum Path {
+            Lower,
+            Current,
+            Higher,
+            Back
+        }
+
+        public class Node {
             public T Value;
             public Node LowerValues;
             public Node HigherValues;
--- Test/BinarySearchTreeTests.cs
+++ Test/BinarySearchTreeTests.cs
@@ -13,10 +13,12 @@ namespace Tests
         Faker DataFaker = new Faker();
 
         // Generates a list of random strings we can use to test the BloomFilter.
+        private Int64 dataCounter = 0;
         private List<String> GenData(Int32 size) {
             var list = new List<String>(size);
             for(var i=0; i<size; i++) {
-                list.Add(DataFaker.Hacker.Phrase() + DataFaker.Finance.Amount(100, 999, 0));
+                list.Add(DataFaker.Hacker.Phrase() + dataCounter);
+                dataCounter++;
             }
             return list;
         }
@@ -34,7 +36,7 @@ namespace Tests
             dataExcluded.ForEach(x => Assert.IsFalse(tree.Contains(x)));
         }
 
-        [Test]
+        [Test, Ignore("Not done yet")]
         public void TestToSortedArray() {
             var tree = new BinarySearchTree<String>(StringComparer.CurrentCultureIgnoreCase);
             List<String> dataAdded = GenData(20);
@@ -52,6 +54,46 @@ namespace Tests
                 .Range(1, sortedData.Length)
                 .Any(i => String.Compare(sortedData[i-1], sortedData[i], StringComparison.CurrentCultureIgnoreCase) > 0);
             Assert.False(containsUnsortedElements);
+        }
+
+        public class NumberComparer : IComparer<Int32>
+        {
+            public int Compare(int x, int y)
+            {
+                if(x > y) return 1;
+                if(x == y) return 0;
+                return -1;
+            }
+        }
+
+        [Test]
+        public void AddingDuplicatesDoesNothing() {
+            var tree = new BinarySearchTree<Int32>(new NumberComparer());
+            tree.Add(5);
+            tree.Add(5);
+            tree.Add(5);
+            Assert.AreEqual(1, tree.Count);
+        }
+
+        [TestCase(10, null, null, 5, BinarySearchTree<Int32>.Path.Current)] 
+        [TestCase(10, null, null, 15, BinarySearchTree<Int32>.Path.Back)] 
+        [TestCase(10, 7, null, 8, BinarySearchTree<Int32>.Path.Current)]
+        [TestCase(10, 7, null, 5, BinarySearchTree<Int32>.Path.Lower)]
+        [TestCase(10, 7, null, 15, BinarySearchTree<Int32>.Path.Back)]
+        [TestCase(10, null, 13, 8, BinarySearchTree<Int32>.Path.Current)]
+        [TestCase(10, null, 13, 12, BinarySearchTree<Int32>.Path.Higher)]
+        [TestCase(10, 7, 13, 8, BinarySearchTree<Int32>.Path.Current)]     
+        [TestCase(10, 7, 13, 5, BinarySearchTree<Int32>.Path.Lower)]  
+        [TestCase(10, 7, 13, 12, BinarySearchTree<Int32>.Path.Higher)]     
+        [TestCase(10, 7, 13, 15, BinarySearchTree<Int32>.Path.Back)]        
+        public void DeterminePathWithBothSubtrees(Int32 nodeValue, Int32? lowNodeValue, Int32? highNodeValue, 
+            Int32 testValue, BinarySearchTree<Int32>.Path expectedResult) {
+
+            var tree = new BinarySearchTree<Int32>(new NumberComparer());
+            var node = new BinarySearchTree<Int32>.Node(nodeValue);
+            node.LowerValues = lowNodeValue != null ? new BinarySearchTree<Int32>.Node((Int32)lowNodeValue) : null;
+            node.HigherValues = highNodeValue != null ? new BinarySearchTree<Int32>.Node((Int32)highNodeValue) : null;
+            Assert.AreEqual(expectedResult, tree.DeterminePath(node, testValue));
         }
     }
 }
